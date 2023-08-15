d98269d1713621e61f10dc26f0e8847ef153b4fb
YangXijie <564197835@qq.com>
2023-08-15 09:13:14 +0800

    remove TemplateProjectPage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
-import { HashRouter, BrowserRouter, Switch, Route } from "react-router-dom";
+import { HashRouter, Switch, Route } from "react-router-dom";
+
 import HomePage from "./page/Home";
 import ResearchPage from "./page/Research";
 import PublicationPage from "./page/Publications";
 import NewsPage from "./page/News";
 import JoinUsPage from "./page/JoinUs";
-
-import TemplateProjectPage from "./projects/template";
 
 class App extends React.Component {
   render() {
@@ -18,11 +17,6 @@ class App extends React.Component {
           <Route exact path="/publication" component={PublicationPage} />
           <Route exact path="/news" component={NewsPage} />
           <Route exact path="/joinus" component={JoinUsPage} />
-          <Route
-            exact
-            path="/projects/template"
-            component={TemplateProjectPage}
-          />
         </Switch>
       </HashRouter>
     );
