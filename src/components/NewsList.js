7bcdd808377301c2cfc67590c37722b34ee67e4c
YangXijie <564197835@qq.com>
2023-08-15 10:08:56 +0800

    better structure of news.json

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -5,14 +5,14 @@ import Box from "@mui/material/Box";
 import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 import Link from "@mui/material/Link";
 
-import News from "../data/news.json";
+import newsData from "../data/news.json";
 
 export default function NewsList() {
   return (
     <div>
       <Box sx={{ marginTop: 10 }}>
-        {News.map((item, i) => (
-          <NewsItem key={i} item={item} />
+        {newsData.map((yearlyNews) => (
+          <NewsItem yearlyNews={yearlyNews} />
         ))}
       </Box>
     </div>
@@ -22,14 +22,11 @@ export default function NewsList() {
 function NewsItem(props) {
   return (
     <Box sx={{ marginBottom: 10 }}>
-      <Typography sx={{ fontSize: 40 }}>{props.item.year}</Typography>
-      {props.item.items.map((line, i) => (
+      <Typography sx={{ fontSize: 40 }}>{props.yearlyNews.year}</Typography>
+      {props.yearlyNews.items.map((item, i) => (
         <Typography sx={{ marginLeft: 3, fontSize: 18, marginBottom: 2 }}>
           <CalendarMonthIcon sx={{ color: "#f44336", marginRight: 2 }} />
-          {line}{" "}
-          {props.item.links[i] !== "" && (
-            <Link href={props.item.links[i]}>more</Link>
-          )}
+          {item.title} {item.link !== "" && <Link href={item.link}>more</Link>}
         </Typography>
       ))}
     </Box>
