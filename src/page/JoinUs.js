e93184ac1e780bc142a465a551f382b5904177a6
YangXijie <564197835@qq.com>
2023-08-15 09:43:17 +0800

    comment research page; better Join Us page

--- src/page/JoinUs.js
+++ src/page/JoinUs.js
@@ -1,8 +1,7 @@
 import * as React from "react";
-import Container from "@mui/material/Container";
+import { Container, Typography } from "@mui/material";
 
 import ElevateAppBar from "../components/ElevateAppbar";
-import { Typography } from "@mui/material";
 
 class JoinUsPage extends React.Component {
   render() {
@@ -10,24 +9,23 @@ class JoinUsPage extends React.Component {
       <div>
         <ElevateAppBar />
         <Container maxWidth="sm">
-          {/* <h1>JoinUs</h1> */}
-          <Typography variant="h6" sx={{ mt: 10, textAlign: "justify" }}>
+          <Typography variant="h6" sx={{ marginTop: 10, textAlign: "justify" }}>
             In IDC group@Shanghai AI Laboratory, we are devoted to advance the
             forefront of research in developing and applying AI-based tools for
             content creation and digitization.
           </Typography>
-
-          <Typography sx={{ mt: 3 }}>
+          <Typography sx={{ marginTop: 3, textAlign: "justify" }}>
             Our mission is to empower users to manifest their imagination in a
             better, more efficient and accessible manner.
           </Typography>
-
-          <Typography sx={{ mt: 1 }}>
+          <Typography sx={{ marginTop: 3, textAlign: "justify" }}>
             We are actively seeking top-tier researchers and interns who are
             driven, disciplined, and innovative to embark on this exciting
             journey.
           </Typography>
-          <Typography sx={{ mt: 3 }}>
+          <Typography
+            sx={{ marginTop: 3, marginBottom: 10, textAlign: "justify" }}
+          >
             If you believe you have what it takes, please do not hesitate to
             express your interest to daibo@pjlab.org.cn.
           </Typography>
