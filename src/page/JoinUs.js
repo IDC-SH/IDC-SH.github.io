da04aa1299d7292f3b3d85972da277c160e9c320
YangXijie <564197835@qq.com>
2023-08-17 14:51:50 +0800

    simplify news

--- src/page/JoinUs.js
+++ src/page/JoinUs.js
@@ -3,22 +3,20 @@ import { Container, Typography } from "@mui/material";
 export default function JoinUsPage() {
   return (
     <Container maxWidth="sm">
-      <Typography variant="h6" sx={{ marginTop: 10, justifyContent: "center" }}>
+      <Typography variant="h6" sx={{ marginTop: 10 }}>
         In IDC Group @ Shanghai AI Laboratory, we are devoted to advance the
         forefront of research in developing and applying AI-based tools for
         content creation and digitization.
       </Typography>
-      <Typography sx={{ marginTop: 2, justifyContent: "center" }}>
+      <Typography sx={{ marginTop: 2 }}>
         Our mission is to empower users to manifest their imagination in a
         better, more efficient and accessible manner.
       </Typography>
-      <Typography sx={{ marginTop: 2, justifyContent: "center" }}>
+      <Typography sx={{ marginTop: 2 }}>
         We are actively seeking top-tier researchers and interns who are driven,
         disciplined, and innovative to embark on this exciting journey.
       </Typography>
-      <Typography
-        sx={{ marginTop: 2, marginBottom: 10, justifyContent: "center" }}
-      >
+      <Typography sx={{ marginTop: 2, marginBottom: 10 }}>
         If you believe you have what it takes, please do not hesitate to express
         your interest to daibo@pjlab.org.cn.
       </Typography>
--- src/page/News.js
+++ src/page/News.js
@@ -1,10 +1,37 @@
-import { Container } from "@mui/material";
+import { Typography, Box, Link, Container, Grid } from "@mui/material";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 
-import NewsList from "../components/NewsList";
+import newsData from "../data/news.json";
+
+function NewsItem({ time, title, link }) {
+  return (
+    <>
+      <Box display="flex" alignItems="center" padding="0">
+        <CalendarMonthIcon sx={{ color: "#f44336", marginRight: 1 }} />
+        <Typography variant="h6">{time}</Typography>
+      </Box>
+      <Typography variant="body1">
+        {title} {link !== "" && <Link href={link}>more</Link>}
+      </Typography>
+    </>
+  );
+}
+
+function NewsList() {
+  return (
+    <>
+      {newsData.map((item) => (
+        <Box sx={{ marginBottom: 2 }}>
+          <NewsItem time={item.time} title={item.title} link={item.link} />
+        </Box>
+      ))}
+    </>
+  );
+}
 
 export default function NewsPage() {
   return (
-    <Container maxWidth="lg">
+    <Container maxWidth="sm" sx={{ marginTop: 10 }}>
       <NewsList />
     </Container>
   );
