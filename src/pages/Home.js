86f65b2f52649ba6a6ffafe3c01ae7a4cf06eeba
YangXijie <564197835@qq.com>
2023-08-18 10:59:35 +0800

    use inline css in Hme.js and remove css file; compress background.jeg

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,71 +6,80 @@ import {
 } from "../components/Highlights";
 import RecentPublicationList from "../components/RecentPublicationList";
 
-import "./App.css";
+import backgroundImage from "./images-home/background.jpeg";
 
 function MainIntroduction() {
   return (
-    <>
-      <Box component="div" className="container" height={900}>
-        <Container maxWidth="lg">
-          <Box
-            component="div"
+    <Box
+      component="div"
+      height={600}
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        backgroundAttachment: "scroll",
+        backgroundSize: "cover",
+        backgroundColor: "black",
+      }}
+    >
+      <Container maxWidth="lg">
+        <Box
+          component="div"
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "flex-start",
+            justifyContent: "center",
+            marginLeft: 10,
+          }}
+        >
+          <Typography
             sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "flex-end",
-              justifyContent: "center",
+              mt: 20,
+              fontStyle: "oblique",
+              color: "common.white",
+              fontSize: 200,
+              lineHeight: 0.9,
             }}
           >
-            {/* <Typography sx={{color:"common.white",lineHeight:0.8}}> */}
-            <Typography
-              sx={{
-                mt: 60,
-                fontStyle: "oblique",
-                color: "common.white",
-                fontSize: 200,
-                lineHeight: 0.9,
-              }}
-            >
-              IDC
-            </Typography>
-            <Typography
-              sx={{
-                mr: 2,
-                fontStyle: "oblique",
-                color: "common.white",
-                fontSize: 28,
-                lineHeight: 0.8,
-              }}
-            >
-              Intelligent Digital Creation
-            </Typography>
-            <Typography
-              sx={{
-                color: "common.white",
-                fontStyle: "oblique",
-                fontSize: 100,
-                lineHeight: 1,
-              }}
-            >
-              Group
-            </Typography>
-            <Typography
-              sx={{
-                mt: 1,
-                mr: 2,
-                fontStyle: "oblique",
-                color: "common.white",
-                fontSize: 12,
-                lineHeight: 0.8,
-              }}
-            >
-              @ Shanghai AI Laboratory
-            </Typography>
-          </Box>
-        </Container>
-      </Box>
-    </>
+            IDC
+          </Typography>
+          <Typography
+            sx={{
+              mr: 2,
+              fontStyle: "oblique",
+              color: "common.white",
+              fontSize: 28,
+              lineHeight: 0.8,
+            }}
+          >
+            Intelligent Digital Creation
+          </Typography>
+          <Typography
+            sx={{
+              color: "common.white",
+              fontStyle: "oblique",
+              fontSize: 100,
+              lineHeight: 1,
+            }}
+          >
+            Group
+          </Typography>
+          <Typography
+            sx={{
+              mt: 1,
+              mr: 2,
+              fontStyle: "oblique",
+              color: "common.white",
+              fontSize: 12,
+              lineHeight: 0.8,
+            }}
+          >
+            @ Shanghai AI Laboratory
+          </Typography>
+        </Box>
+      </Container>
+    </Box>
   );
 }
 
