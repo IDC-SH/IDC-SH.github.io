a295600dc5a8dec33e0c86123f50e4219cc9c9fb
YangXijie <564197835@qq.com>
2023-08-18 14:08:56 +0800

    initially add Introduction

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,85 +8,134 @@ import { RecentPublicationList } from "../components/PublicationList";
 
 import backgroundImage from "./images-home/background.jpeg";
 
-function MainIntroduction() {
+function LabIntroduction() {
+  const textStyle = {
+    fontStyle: "oblique",
+    color: "white",
+    whiteSpace: "nowrap",
+    fontWeight: "bold",
+  };
   return (
     <Box
-      component="div"
-      height={600}
-      style={{
-        backgroundImage: `url(${backgroundImage})`,
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "center",
-        backgroundAttachment: "scroll",
-        backgroundSize: "cover",
-        backgroundColor: "black",
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
       }}
     >
-      <Container maxWidth="lg">
-        <Box
-          component="div"
+      <Typography
+        sx={{ ...textStyle, fontSize: 160, lineHeight: 1, letterSpacing: 20 }}
+      >
+        IDC
+      </Typography>
+      <Typography sx={{ ...textStyle, fontSize: 20, lineHeight: 0.6 }}>
+        Intelligent Digital Creation
+      </Typography>
+      <Typography sx={{ ...textStyle, fontSize: 120, lineHeight: 1.2 }}>
+        Group
+      </Typography>
+      <Typography sx={{ ...textStyle, fontSize: 18, lineHeight: 0.8 }}>
+        @ Shanghai AI Laboratory
+      </Typography>
+    </Box>
+  );
+}
+
+function RecruitingIntroduction() {
+  const textStyle = {
+    fontStyle: "oblique",
+    color: "white",
+    whiteSpace: "nowrap",
+  };
+
+  return (
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Typography
           sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "flex-start",
-            justifyContent: "center",
-            marginLeft: 10,
+            ...textStyle,
+            fontFamily: "Comic Sans MS",
+            fontSize: 64,
+            lineHeight: 2.2,
           }}
         >
-          <Typography
-            sx={{
-              mt: 20,
-              fontStyle: "oblique",
-              color: "common.white",
-              fontSize: 200,
-              lineHeight: 0.9,
-            }}
-          >
-            IDC
-          </Typography>
-          <Typography
-            sx={{
-              mr: 2,
-              fontStyle: "oblique",
-              color: "common.white",
-              fontSize: 28,
-              lineHeight: 0.8,
-            }}
-          >
-            Intelligent Digital Creation
-          </Typography>
-          <Typography
-            sx={{
-              color: "common.white",
-              fontStyle: "oblique",
-              fontSize: 100,
-              lineHeight: 1,
-            }}
-          >
-            Group
-          </Typography>
-          <Typography
-            sx={{
-              mt: 1,
-              mr: 2,
-              fontStyle: "oblique",
-              color: "common.white",
-              fontSize: 12,
-              lineHeight: 0.8,
-            }}
-          >
-            @ Shanghai AI Laboratory
-          </Typography>
+          Team is Recruiting
+        </Typography>
+        <Typography
+          variant="subtitle2"
+          sx={{
+            fontSize: 28,
+            lineHeight: 2,
+            color: "white",
+            textAlign: "center",
+          }}
+        >
+          If you believe you have what it takes,
+          <br />
+          please do not hesitate to express
+          <br />
+          your interest to <i>daibo@pjlab.org.cn</i>
+        </Typography>
+      </Box>
+    </>
+  );
+}
+
+function MainIntroduction() {
+  return (
+    <Container maxWidth="lg">
+      <Box display="flex">
+        <Box
+          sx={{
+            height: 400,
+            backgroundColor: "rgba(232,232,232,0.2)",
+            marginTop: 12,
+            marginLeft: 5,
+            marginRight: 5,
+            padding: 4,
+          }}
+        >
+          <LabIntroduction />
         </Box>
-      </Container>
-    </Box>
+        <Box
+          sx={{
+            height: 400,
+            backgroundColor: "rgba(232,232,232,0.2)",
+            marginTop: 12,
+            marginRight: 5,
+            padding: 4,
+          }}
+        >
+          <RecruitingIntroduction />
+        </Box>
+      </Box>
+    </Container>
   );
 }
 
 export default function HomePage() {
   return (
     <>
-      <MainIntroduction />
+      <Box
+        height={600}
+        style={{
+          backgroundImage: `url(${backgroundImage})`,
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          backgroundAttachment: "scroll",
+          backgroundSize: "cover",
+          backgroundColor: "black",
+        }}
+      >
+        <MainIntroduction />
+      </Box>
 
       <Divider sx={{ margin: 4 }}>
         <Typography variant="h2" textAlign="center">
