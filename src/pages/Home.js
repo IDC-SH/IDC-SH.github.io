9375d835acb4cf3fb642e922b5c2824fcc400410
YangXijie <564197835@qq.com>
2024-03-08 12:35:02 +0800

    add latest news

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,8 @@ import { Typography, Divider, Box } from "@mui/material";
 import HighlightSection from "../components/HighlightSection";
 import { RecentPublicationList } from "../components/PublicationList";
 // import { Form } from "react-router-dom";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import { Container } from "@mui/system";
 
 function LabIntroduction() {
   const textStyle1 = {
@@ -93,10 +95,91 @@ function LabIntroduction() {
   );
 }
 
+function NewsItem({ time, title, link }) {
+  return (
+    <>
+      <Container>
+        <Box display="flex" justifyContent="center" padding="0">
+          {/* <CalendarMonthIcon
+            sx={{ color: "#f44336", marginX: 1, fontSize: "29px" }}
+          /> */}
+          <Typography
+            variant="h5"
+            sx={{ fontSize: "24px", marginX: 1, fontWeight: "bold" }}
+          >
+            🎉
+          </Typography>
+          <Typography
+            variant="h5"
+            sx={{ fontSize: "24px", marginX: 1, fontWeight: "bold" }}
+          >
+            {time}
+          </Typography>
+          <Typography
+            variant="h5"
+            sx={{
+              fontSize: "24px",
+              marginX: 1,
+              fontWeight: "bold",
+              fontStyle: "italic",
+            }}
+          >
+            {title}
+          </Typography>
+          <Typography
+            variant="h5"
+            sx={{ fontSize: "24px", marginX: 1, fontWeight: "bold" }}
+          >
+            🎉
+          </Typography>
+        </Box>
+      </Container>
+    </>
+  );
+}
+
 export default function HomePage() {
   return (
     <>
       <LabIntroduction />
+
+      <Divider sx={{ margin: 4 }}>
+        <Typography
+          variant="h2"
+          sx={{
+            fontWeight: "bold",
+            fontFamily: "Roboto Condensed",
+            color: "rgb(53,73,183)",
+            textAlign: "center",
+            position: "relative",
+            "&::after": {
+              content: '""',
+              position: "absolute",
+              bottom: 0,
+              left: 0,
+              width: "100%",
+              height: "50%",
+              background: "rgb(237,195,71)",
+              zIndex: -1,
+            },
+            "&:hover::after": {
+              top: 0,
+              bottom: "auto",
+              height: "50%",
+              background: "rgb(237,195,71)",
+            },
+          }}
+        >
+          Latest News
+        </Typography>
+      </Divider>
+
+      <Box sx={{ marginBottom: 2 }}>
+        <NewsItem
+          time={"02/2024"}
+          title={"Ten papers are accepted to CVPR 2024."}
+        />
+      </Box>
 
       <Divider sx={{ margin: 4 }}>
         <Typography
