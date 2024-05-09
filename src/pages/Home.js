bf2edd6dd770f97604478d223e08f9998993d949
YangXijie <564197835@qq.com>
2024-05-09 20:21:21 +0800

    refactor DoubleColorTitle

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -98,52 +98,91 @@ function LabIntroduction() {
 function NewsItem({ time, title, link }) {
   return (
     <>
-      <Container>
-        <Box display="flex" justifyContent="center" padding="0">
-          {/* <CalendarMonthIcon
+      <Box sx={{ marginBottom: 2 }}>
+        <Container>
+          <Box display="flex" justifyContent="center" padding="0">
+            {/* <CalendarMonthIcon
             sx={{ color: "#f44336", marginX: 1, fontSize: "29px" }}
           /> */}
-          <Typography
-            variant="h5"
-            sx={{ fontSize: "24px", marginX: 1, fontWeight: "bold" }}
-          >
-            🎉
-          </Typography>
-          <Typography
-            variant="h5"
-            sx={{ fontSize: "24px", marginX: 1, fontWeight: "bold" }}
-          >
-            {time}
-          </Typography>
-          <Typography
-            variant="h5"
-            sx={{ fontSize: "24px", marginX: 1, fontWeight: "bold" }}
-          >
-            🎉
-          </Typography>
-        </Box>
-        {/* fix for mobile: use two lines for one news */}
-        <Box display="flex" justifyContent="center" padding="0">
-          <Typography
-            variant="h5"
-            align="center"
-            sx={{
-              fontSize: "20px",
-              marginX: 1,
-              fontWeight: "bold",
-              fontStyle: "italic",
-            }}
-          >
-            {link ? (
-              <Link href={link} underline="none" color="inherit">
-                {title}
-              </Link>
-            ) : (
-              title
-            )}
-          </Typography>
-        </Box>
-      </Container>
+            <Typography
+              variant="h5"
+              sx={{ fontSize: "24px", marginX: 1, fontWeight: "bold" }}
+            >
+              🎉
+            </Typography>
+            <Typography
+              variant="h5"
+              sx={{ fontSize: "24px", marginX: 1, fontWeight: "bold" }}
+            >
+              {time}
+            </Typography>
+            <Typography
+              variant="h5"
+              sx={{ fontSize: "24px", marginX: 1, fontWeight: "bold" }}
+            >
+              🎉
+            </Typography>
+          </Box>
+          {/* fix for mobile: use two lines for one news */}
+          <Box display="flex" justifyContent="center" padding="0">
+            <Typography
+              variant="h5"
+              align="center"
+              sx={{
+                fontSize: "20px",
+                marginX: 1,
+                fontWeight: "bold",
+                fontStyle: "italic",
+              }}
+            >
+              {link ? (
+                <Link href={link} underline="none" color="inherit">
+                  {title}
+                </Link>
+              ) : (
+                title
+              )}
+            </Typography>
+          </Box>
+        </Container>
+      </Box>
+    </>
+  );
+}
+
+function DoubleColorTitle({ title }) {
+  return (
+    <>
+      <Divider sx={{ marginX: 0, marginY: 4 }}>
+        <Typography
+          variant="h2"
+          sx={{
+            fontWeight: "bold",
+            fontFamily: "Roboto Condensed",
+            color: "rgb(53,73,183)",
+            textAlign: "center",
+            position: "relative",
+            "&::after": {
+              content: '""',
+              position: "absolute",
+              bottom: 0,
+              left: 0,
+              width: "100%",
+              height: "50%",
+              background: "rgb(237,195,71)",
+              zIndex: -1,
+            },
+            "&:hover::after": {
+              top: 0,
+              bottom: "auto",
+              height: "50%",
+              background: "rgb(237,195,71)",
+            },
+          }}
+        >
+          {title}
+        </Typography>
+      </Divider>
     </>
   );
 }
@@ -153,117 +192,26 @@ export default function HomePage() {
     <>
       <LabIntroduction />
 
-      <Divider sx={{ marginX: 0, marginY: 4 }}>
-        <Typography
-          variant="h2"
-          sx={{
-            fontWeight: "bold",
-            fontFamily: "Roboto Condensed",
-            color: "rgb(53,73,183)",
-            textAlign: "center",
-            position: "relative",
-            "&::after": {
-              content: '""',
-              position: "absolute",
-              bottom: 0,
-              left: 0,
-              width: "100%",
-              height: "50%",
-              background: "rgb(237,195,71)",
-              zIndex: -1,
-            },
-            "&:hover::after": {
-              top: 0,
-              bottom: "auto",
-              height: "50%",
-              background: "rgb(237,195,71)",
-            },
-          }}
-        >
-          Latest News
-        </Typography>
-      </Divider>
+      <DoubleColorTitle title={"Latest News"} />
 
-      <Box sx={{ marginBottom: 2 }}>
-        <NewsItem
-          time={"04/2024"}
-          title={
-            "LandMark 2.0 is coming! Check the news at shlab.org.cn/news/5443886."
-          }
-          link={"https://www.shlab.org.cn/news/5443886"}
-        />
-      </Box>
+      <NewsItem
+        time={"04/2024"}
+        title={
+          "LandMark 2.0 is coming! Check the news at shlab.org.cn/news/5443886."
+        }
+        link={"https://www.shlab.org.cn/news/5443886"}
+      />
 
-      <Box sx={{ marginBottom: 2 }}>
-        <NewsItem
-          time={"02/2024"}
-          title={"Ten papers are accepted to CVPR 2024."}
-        />
-      </Box>
+      <NewsItem
+        time={"02/2024"}
+        title={"Ten papers are accepted to CVPR 2024."}
+      />
 
-      <Divider sx={{ marginX: 0, marginY: 4 }}>
-        <Typography
-          variant="h2"
-          sx={{
-            fontWeight: "bold",
-            fontFamily: "Roboto Condensed",
-            color: "rgb(53,73,183)",
-            textAlign: "center",
-            position: "relative",
-            "&::after": {
-              content: '""',
-              position: "absolute",
-              bottom: 0,
-              left: 0,
-              width: "100%",
-              height: "50%",
-              background: "rgb(237,195,71)",
-              zIndex: -1,
-            },
-            "&:hover::after": {
-              top: 0,
-              bottom: "auto",
-              height: "50%",
-              background: "rgb(237,195,71)",
-            },
-          }}
-        >
-          Highlights
-        </Typography>
-      </Divider>
+      <DoubleColorTitle title={"Highlights"} />
 
       <HighlightSection />
 
-      <Divider sx={{ marginX: 0, marginY: 4 }}>
-        <Typography
-          variant="h2"
-          sx={{
-            fontWeight: "bold",
-            fontFamily: "Roboto Condensed",
-            color: "rgb(53,73,183)",
-            textAlign: "center",
-            position: "relative",
-            "&::after": {
-              content: '""',
-              position: "absolute",
-              bottom: 0,
-              left: 0,
-              width: "100%",
-              height: "50%",
-              background: "rgb(237,195,71)",
-              zIndex: -1,
-            },
-            "&:hover::after": {
-              top: 0,
-              bottom: "auto",
-              height: "50%",
-              background: "rgb(237,195,71)",
-            },
-          }}
-        >
-          Recents
-        </Typography>
-      </Divider>
+      <DoubleColorTitle title={"Recents"} />
 
       <RecentPublicationList />
     </>
